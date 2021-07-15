5fd681ad4c2cca47c9ed0e052802f1a58ba1dfd7
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2021-07-15 14:49:03 +0200

    add wrapper Main class for cross-platform fat jar

---
+++ src/main/java/me/frauenfelderflorian/croquet/Main.java
@@ -0,0 +1,10 @@
+package me.frauenfelderflorian.croquet;
+
+/**
+* This class is only  used for cross-platform fat jars.
+ */
+public class Main {
+    public static void main(String[] args) {
+        CroquetApp.main(args);
+    }
+}
