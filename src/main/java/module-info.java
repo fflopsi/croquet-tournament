9ceb6c524401006f07e0982c1de53e2a46ea4797
Florian Frauenfelder <florian.l.frauenfelder@gmail.com>
2021-07-15 15:05:40 +0200

    update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,8 @@ module me.frauenfelderflorian.croquet {
     requires javafx.fxml;
     requires java.prefs;
 
-    opens me.frauenfelderflorian.croquet to javafx.fxml;
     exports me.frauenfelderflorian.croquet;
-    exports me.frauenfelderflorian.croquet.data;
-    opens me.frauenfelderflorian.croquet.data to javafx.fxml;
+    opens me.frauenfelderflorian.croquet to javafx.fxml;
+    //exports me.frauenfelderflorian.croquet.data;
+    //opens me.frauenfelderflorian.croquet.data to javafx.fxml;
 }
